2861c33f7a878cb180b08ed80095c69d778ed18b
Jan Poniatowski <poniatowski126@gmail.com>
2024-04-19 18:27:12 +0200

    feat: api routes

--- app/api/github/webhooks/check-suits/route.js
+++ app/api/github/webhooks/check-suits/route.js
@@ -1,6 +1,6 @@
 export async function POST(request) {
   const body = await request.json();
-  console.log(body);
+  console.log(body.action, new Date());
 
   return Response.json({ message: "ok" });
 }
