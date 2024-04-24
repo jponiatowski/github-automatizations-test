8070e31b7c56881161bd847666d50ae1477a7e65
Jan Poniatowski <poniatowski126@gmail.com>
2024-04-24 11:38:47 +0200

    feat: release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,63 @@
-async function release() {}
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+  auth: "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",
+});
+
+const versionRegex = new RegExp(/^\d+\.\d+\.\d+$/);
+
+async function release() {
+  const version = process.argv?.[2];
+  console.log(process.argv);
+  if (!version) {
+    throw new Error("No version provided");
+  }
+
+  if (!versionRegex.test(version)) {
+    throw new Error("Invalid version format");
+  }
+
+  // Get master branch sha
+  const masterBranch = await octokit.rest.repos.getBranch({
+    owner: "jponiatowski",
+    repo: "github-automatizations-test",
+    branch: "master",
+  });
+
+  const sha = masterBranch?.data?.commit?.sha;
+
+  if (!sha) {
+    throw new Error("No sha found for master branch");
+  }
+
+  // Create release branch
+  const releaseBranch = await octokit.rest.git.createRef({
+    owner: "jponiatowski",
+    repo: "github-automatizations-test",
+    ref: `refs/heads/release/${version}`,
+    sha,
+  });
+
+  if (releaseBranch.status !== 201) {
+    throw new Error("Creating release branch failed");
+  }
+
+  console.log("Release branch created");
+
+  // Create dev to release branch PR
+  const pullRequest = await octokit.rest.pulls.create({
+    owner: "jponiatowski",
+    repo: "github-automatizations-test",
+    title: `Release branch ${version}`,
+    head: "dev",
+    base: `release/${version}`,
+  });
+
+  if (pullRequest.status !== 201) {
+    throw new Error("Creating pull request failed");
+  }
+
+  console.log("Pull request created created");
+}
 
 release();
